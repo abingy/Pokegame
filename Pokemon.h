0447f023e971bb110258e7fe8ef630db9c23aa8a
abingy <abingy@umich.edu>
2017-05-10 00:32:01 -0400

    editted Species.h and Pokemon.h

--- Pokemon.h
+++ Pokemon.h
@@ -1,2 +1,4 @@
 #ifndef _POKEMON_H_
 #define _POKEMON_H_
+
+#endif //_POKEMON_H_
--- Species.h
+++ Species.h
@@ -1,2 +1,20 @@
 #ifndef _SPECIES_H_
 #define _SPECIES_H_
+
+#endif // _SPECIES_H_
+
+enum Type {
+    None, Normal, Fighting, Flying, Poison, Ground, Rock, Bug, Ghost, Steel,
+    Fire, Water, Grass, Electric, Psychic, Ice, Dragon, Dark, Fairy
+};
+
+enum Rarity {
+    Common, Uncommon, Rare, Legendary
+};
+
+class Species
+{
+    Species();
+    
+    Species();
+};
